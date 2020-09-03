92937f8264791a9069e55c8e0824a5d36aaf167f
Nelson Rodrigues <nelsoncuia23@gmail.com>
2020-09-03 11:55:54 -0300

    first commit

---
+++ example.js
@@ -0,0 +1,17 @@
+var SignedXml = require('xml-crypto').SignedXml	  
+      , fs = require('fs')
+ 
+var xml = "<library>" +
+                "<book>" +
+                  "<name>Harry Potter</name>" +
+                "</book>" +
+              "</library>"
+ 
+
+var sig = new SignedXml()
+sig.digestAlgorithm  = "http://www.w3.org/2001/04/xmlenc#sha256"
+sig.signatureAlgorithm = "http://www.w3.org/2001/04/xmldsig-more#rsa-sha256"
+sig.addReference("//*[local-name(.)='library']", ['http://www.w3.org/TR/2001/REC-xml-c14n-20010315'], 'http://www.w3.org/2001/04/xmlenc#sha256')    
+sig.signingKey = fs.readFileSync("key.pem")
+sig.computeSignature(xml)
+fs.writeFileSync("signed3.xml", sig.getSignedXml())
